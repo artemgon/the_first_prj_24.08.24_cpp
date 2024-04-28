4e637365e96fd600a4eb283033b4a22af5e1f9cd
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 21:00:00 +0300

    Creating the first task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
+	int number;
+	cout << "Please, enter any number you want:";
+	cin >> number;
+	cout << "Your number is ";
+	(number % 2 == 0) ? cout << "even" : cout << "odd";
 	return 0;
 }
