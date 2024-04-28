bd51e9999d61fe8bdbcedb6a1b8125b07276dd56
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 21:08:48 +0300

    Creating the fourth task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+	int number_1;
+	int number_2;
+	cout << "Please, enter the first number:";
+	cin >> number_1;
+	cout << "Please, enter the second number:";
+	cin >> number_2;
+	if (number_1 > number_2 || number_1 < number_2) {
+		cout << "The numbers are not the same\n";
+		if (number_1 > number_2) {
+			cout << "Number 1 is bigger";
+		}
+		else {
+			cout << "Number 2 is bigger";
+		}
+	}
+	else
+	{
+		cout << "The numbers are the same";
+	}
 	return 0;
 }
