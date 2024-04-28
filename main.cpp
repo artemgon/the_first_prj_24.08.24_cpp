c887dd6f0f5dbd923bbacedb9bb47eb4abcb8487
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 21:06:19 +0300

    Creating the third task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+	int number;
+	cout << "Please, enter any number you want:";
+	cin >> number;
+	if (number > 0) {
+		cout << "Your number is bigger than 0";
+	}
+	else if (number < 0) {
+		cout << "Your number is less than 0";
+	}
+	else {
+		cout << "Your number equals to 0";
+	}
 	return 0;
 }
