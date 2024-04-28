89a4f3698be1991e487d4719217caebe0e9fe964
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 21:35:28 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+	int number;
+	cout << "Please, enter a 6-digits number(type:int):";
+	cin >> number;
+	if ((number / 100000 % 10 + number / 10000 % 10 + number / 1000 % 10) == (number / 100 % 10 + number / 10 % 10 + number % 10)) {
+		cout << "Your number is happy";
+	}
+	else if (100000 - number > 0){
+		cout << "The number you've entered isn't 6-digits";
+	}
+	else {
+		cout << "Unfortunately, your number isn't happy";
+	}
 	return 0;
 }
