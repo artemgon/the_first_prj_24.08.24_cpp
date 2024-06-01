7bca36221197c4c64a020b55fa0d5213422e9cce
artemgon <artemhoncharuk123@gmail.com>
2024-06-01 17:32:26 +0300

    Creating the third task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,32 @@
 using namespace std;
 
 int main() {
+	int a, b, c, d, e, f, g, max_number = 0;
+	cin >> a >> b >> c >> d >> e >> f >> g;
+	if (a > b && a > c)
+	{
+		max_number = a;
+	}
+	else if (b > a && b > c) {
+		max_number = b;
+	}
+	else {
+		max_number = c;
+	}
+	if (max_number > d && max_number < e)
+	{
+		max_number = d;
+	}
+	else {
+		max_number = e;
+	}
+	if (max_number < f && f > g)
+	{
+		max_number = f;
+	}
+	else {
+		max_number = g;
+	}
+	cout << "The biggest number = " << max_number;
 	return 0;
 }
